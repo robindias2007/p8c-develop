c1babf02e07b763783bc0e6c13c440cbbc78ff4c
robin <robindias2007@gmail.com>
2016-11-29 17:49:35 +0530

    few changes

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,4 +1,4 @@
 class Form < ActiveRecord::Base
-validates :description, length: { maximum: 300 }
-
+validates :description, length: { maximum: 200 }
+validates :title, length: { maximum: 50 }          
 end
