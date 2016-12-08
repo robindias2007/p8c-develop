bdd968ea8092221b780639772c37699a4cec12e0
robin <robindias2007@gmail.com>
2016-12-08 17:31:27 +0530

    message

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :forms
   get 'static_pages/home'
   get 'static_pages/home1'
+  root 'static_pages#thanks'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
