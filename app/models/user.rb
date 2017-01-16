dd95fe274ead43ab74275592935e017e8a552824
robin <robindias2007@gmail.com>
2017-01-16 18:13:30 +0530

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  acts_as_voter
+ 
   has_many :forms
   
   
