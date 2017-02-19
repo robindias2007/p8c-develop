4d9011a963f112069772e4476904728e1fcdb7fb
robin <robindias2007@gmail.com>
2017-02-19 17:29:18 +0530

    changes

--- app/models/form.rb
+++ app/models/form.rb
@@ -6,21 +6,21 @@ acts_as_votable
 belongs_to :user
 
 
-validates :description, length: { maximum: 200 }
-validates :title, :title1, :title2, :title3, :titel4, :title5 , length: { maximum: 50 }
+# validates :description, length: { maximum: 200 }
+# validates :title, :title1, :title2, :title3, :titel4, :title5 , length: { maximum: 50 }
 
 
 
-before_validation :smart_add_url_protocol 
+# before_validation :smart_add_url_protocol 
 
 
 
-protected
+# protected
 
-def smart_add_url_protocol
-  unless self.url1[/\Ahttp:\/\//] || self.url1[/\Ahttps:\/\//]
-    self.url1 = "http://#{self.url1}"
-  end
-end
+# def smart_add_url_protocol
+#   unless self.url1[/\Ahttp:\/\//] || self.url1[/\Ahttps:\/\//]
+#     self.url1 = "http://#{self.url1}"
+#   end
+# end
 
 end
