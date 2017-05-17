79f650ea1e204ce87e377964b2a4bc6c571d7ab0
robin dias <robindias2007@gmail.com>
2017-05-17 19:43:59 +0530

    user profile styling

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,12 +1,12 @@
 class FollowsController < ApplicationController
 
   def follow
-    @user = User.find_by_username(params[:id])
+    @user = User.find_by(params[:id])
     current_user.follow(@user)
   end
 
   def unfollow
-    @user = User.find_by_username(params[:id])
+    @user = User.find_by(params[:id])
     current_user.stop_following(@user)
   end
 
