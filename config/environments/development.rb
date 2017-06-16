58c3550afa0f4400262efe59cf70c117556b8273
prakash subramani <prakash@mallow-tech.com>
2017-06-16 10:47:40 +0530

    Added `letter_opener` gem for viewing emails in browser.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,16 +38,7 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :user_name            => 'robindias2007@gmail.com',
-      :password             => '9892228035',
-      :authentication       => 'plain',
-      :enable_starttls_auto => true  }
-
-
+  config.action_mailer.delivery_method = :letter_opener
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
