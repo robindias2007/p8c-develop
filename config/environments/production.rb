4790aa20c502ee143caeb43e4da76212c56e9bcc
robin dias <robindias2007@gmail.com>
2017-08-14 12:45:01 +0530

    changes to migration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'lense-mvp73.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'lense-master73.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
