f3a3a13cf0809a919c2ed37b7ec6feb3e2eab656
Malav <malav1410@gmail.com>
2017-09-18 18:18:59 +0530

    keys removed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,8 +44,8 @@ Rails.application.configure do
     :storage => :s3,
     :s3_credentials => {
       :bucket => 'thecurativ-development',
-      :access_key_id => 'AKIAIWQ5GLXQHE5ID6GA',
-      :secret_access_key => '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',
+      :access_key_id => '',
+      :secret_access_key => '',
       :s3_region => 'us-east-2'
     }
   }
