9dd87e506a970480ee7f16e610d5dc8e1a5ab683
Malav <malav1410@gmail.com>
2017-09-19 13:18:33 +0530

    added meta with correct link and pointer on drag and drop

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,6 @@
 //= require ng-infinite-scroll.min
 //= require_tree .
 //= require bootstrap-sprockets
-//= require rails.validations
 //= require bootstrap-tagsinput
 //= require moment
 //= require bootstrap-datetimepicker
