99c9a23433d3c80fe91b62841c2ec2ef39d622aa
robin dias <robindias2007@gmail.com>
2017-10-12 16:51:35 +0530

    loading fontss

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,8 @@ Rails.application.config.assets.precompile += %w( rails.validations.js )
 
 # Add additional assets to the asset load path
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts", "vendor", "bower_components")
-
+Rails.application.config.assets.precompile += %w(.svg .eot .woff .ttf .otf)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( material_icons.css.erb form_social_image.scss)
+
