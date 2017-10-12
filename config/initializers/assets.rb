00af996cd658613c6b418a09aa6b3602ada22e0e
robin dias <robindias2007@gmail.com>
2017-10-12 17:03:50 +0530

    font testing

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,8 @@
 Rails.application.config.assets.precompile += %w( rails.validations.js )
 
 # Add additional assets to the asset load path
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts", "vendor", "bower_components")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "vendor", "bower_components")
+Rails.application.config.assets.paths << Rails.root.join("fonts")
 Rails.application.config.assets.precompile += %w(.svg .eot .woff .ttf .otf)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
