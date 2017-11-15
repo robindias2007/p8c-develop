3dfa7daeb0cd0bac80685e5230a9d60b39074ef2
Malav <malav1410@gmail.com>
2017-11-15 15:50:18 +0530

    reduce load time by fixing css try-1

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,12 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.precompile += %w( rails.validations.js )
+Rails.application.config.assets.precompile += %w( rails.validations.js material_icons.css form_social_image.css)
 
 # Add additional assets to the asset load path
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts", "vendor", "bower_components")
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf)$/
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( material_icons.css.erb form_social_image.scss)
-
